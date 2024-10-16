2974b30374ec792f1dfdb6c06a30fbc27065e437
GuyomeC <146094089+GuyomeC@users.noreply.github.com>
2024-10-16 12:26:20 +0200

    [UPDATE] Moving Platforms

---
+++ DreamTeam/Assets/Script/MovingPlatforms.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class MovingPlatforms : MonoBehaviour
+{
+    public float speed = 2f; // Vitesse de déplacement
+    [SerializeField, Range(0.1f, 50f)] private float limiteDroite = 1f;
+    [SerializeField, Range(0.1f, 50f)] private float limiteGauche = 1f;
+    private Vector3 limiteDroitePosition;
+    private Vector3 limiteGauchePosition;
+    public float distance = 3f; // Distance du mouvement
+    private Vector3 startPosition;
+    private bool movingRight = true;
+
+    void Start()
+    {
+        // Enregistrer la position de départ
+        startPosition = transform.position;
+        limiteDroitePosition = transform.position + new Vector3(limiteDroite, 0, 0);
+        limiteGauchePosition = transform.position - new Vector3(limiteGauche, 0, 0);
+    }
+
+    void Update()
+    {
+        // Faire bouger la plateforme à gauche et à droite
+        if (movingRight)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+
+            // Si la plateforme atteint la distance maximale, inverser le sens
+            if (transform.position.x >= limiteDroite)
+            {
+                movingRight = false;
+            }
+        }
+        else
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+
+            // Si la plateforme atteint la distance minimale, inverser le sens
+            if (-transform.position.x >= limiteGauche)
+            {
+                movingRight = true;
+            }
+        }
+    }
+
+    //Cette fonction sert a visualiser le chemin de l'ennemi dans l'éditeur
+    void OnDrawGizmos()
+    {
+        if (!Application.IsPlaying(gameObject))
+        {
+            limiteDroitePosition = transform.position + new Vector3(limiteDroite, 0, 0);
+            limiteGauchePosition = transform.position - new Vector3(limiteGauche, 0, 0);
+        }
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(limiteDroitePosition, new Vector3(0.2f, 1, 0.2f));
+        Gizmos.DrawCube(limiteGauchePosition, new Vector3(0.2f, 1, 0.2f));
+        Gizmos.DrawLine(limiteDroitePosition, limiteGauchePosition);
+    }
+}
